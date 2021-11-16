d9ae5cb4ad708ee5977354fbe5f452b7cf9c77e3
Ayush Nilabh <ayushnilabhsingh1986@gmail.com>
2021-11-16 21:23:30 +0530

    First Commit...

---
+++ src/comm.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author singh
+ *
+ */
+import java.sql.*;
+
+public class comm {
+Connection c;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
